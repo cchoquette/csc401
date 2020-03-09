9101ac19fe2ee61e8292867b1592b5e9e40bd379
Christopher Choquette Choo <choquette.christopher@gmail.com>
2020-03-08 23:57:10 -0400

    testing

--- a2/code/a2_encoder_decoder.py
+++ a2/code/a2_encoder_decoder.py
@@ -277,7 +277,7 @@ class EncoderDecoder(EncoderDecoderBase):
             l, h_tilde_tm1 = self.decoder.forward(E[t], h_tilde_tm1, h, F_lens)
             logits.append(l)
         logits = torch.stack(logits, 0)
-        return logits
+        return torch.log(logits)
 
     def update_beam(self, htilde_t, b_tm1_1, logpb_tm1, logpy_t):
         # perform the operations within the psuedo-code's loop in the
