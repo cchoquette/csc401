63f5358c7153a66ef15b0588def3f2afe7d5a193
Christopher Choquette Choo <choquette.christopher@gmail.com>
2020-04-07 22:26:54 -0400

    slurm training script

---
+++ a3/code/train_gmm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=01-00:00                       # time (DD-HH:MM)
+#SBATCH --gres=gpu:1                         # Number of GPUs (per node)
+#SBATCH --cpus-per-task=1                    # CPU cores/threads
+#SBATCH --mem=16G                            # memory per node -> use powers of 2
+#SBATCH --qos=normal                         # ICML
+#SBATCH --partition=gpu
+
+eval "$(conda shell.bash hook)"
+conda activate csc401a2
+
+. /etc/profile.d/lmod.sh
+module use /pkgs/environment-modules/
+
+python -u a3_gmm.py
