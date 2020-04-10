89458253823067369234427328a037e073b0ef72
Christopher Choquette Choo <choquette.christopher@gmail.com>
2020-04-10 15:16:53 -0400

    update

--- a3/code/a3_levenshtein.py
+++ a3/code/a3_levenshtein.py
@@ -57,100 +57,59 @@ def Levenshtein(r, h):
     # following the forward-backward algorithm, this is the forward part.
     for i in range(R.shape[0] - 1):
         for j in range(R.shape[1] - 1):
-            i_1 = i + 1
+            i_1 = i + 1  # next indices
             j_1 = j + 1
-            same = int(r[i_1] == h[j_1])
-            sub = R[i, j] - same
+            same = int(r[i_1] == h[j_1])  # if we have a potential substitution
+            sub = R[i, j] - same  # if they are the same (no sub) this will
+            # cancel with the + 1 later.
             delete = R[i, j_1]
             insert = R[i_1, j]
+            # the minimum cost is always taken.
             R[i_1, j_1] = np.array([delete, insert, sub]).min() + 1
-    counts = {0: 0, 1: 0, 2: 0}
+    counts = {0: 0, 1: 0, 2: 0}  # 0: delete, 1: insert, 2: substitution
     i = R.shape[0] - 1
     j = R.shape[1] - 1
+    # now the backward part
     while i > 0 or j > 0:
-        i_1, j_1 = i-1, j-1
-        curr_R = R[i, j]
+        i_1, j_1 = i-1, j-1  # next indices
+        curr_R = R[i, j]  # common numpy accesses
         next_R = R[i_1, j_1]
+        # check sub
         if i > 0 and j > 0 and (next_R == curr_R or next_R == curr_R - 1):
             i -= 1
             j -= 1
             if next_R == curr_R - 1:
                 counts[2] += 1
+        # check insert
         elif j > 0 and R[i, j - 1] == curr_R - 1:
             counts[1] += 1
             j -= 1
+        # check delete
         elif i > 0 and R[i - 1, j] == curr_R - 1:
             counts[0] += 1
             i -= 1
-
+    # return, WER, followed by counts, mine are in reverse order
     return [R[-1, -1] / (R.shape[0] - 2)] + [counts[i] for i in reversed(range(3))]
 
 
-# # create and initialize our grids
-#     # dim 0: for r
-#     # dim 1: for h
-#     # dim 2: 2D where 0 index is the distance and 1 index is the type
-#     # types are 0: delete, 1: insert, 2: substitute
-#     R = np.zeros((len(r) + 2, len(h) + 2))
-#     R[0, :] = np.arange(R.shape[1])
-#     R[:, 0] = np.arange(R.shape[0])
-#     # R[0, :, 1] = np.ones_like(R[0, :, 1])  # inserts
-#     # R[:, 0, 1] = np.zeros_like(R[:, 0, 1])  # deletes
-#     # now setup strings for comparison
-#     r = add_tags(r)
-#     h = add_tags(h)
-#     # following the forward-backward algorithm, this is the forward part.
-#     print(R.shape)
-#     for i in range(R.shape[0]-1):
-#         for j in range(R.shape[1]-1):
-#             j_1 = j+1
-#             i_1 = i+1
-#             same = int(r[i_1] == h[j_1])
-#             delete = R[i, j_1]
-#             insert = R[i_1, j]
-#             substitute = R[i, j] - same  # if they were the same, will cancel with the +1 later
-#             choices = np.array([delete, insert, substitute]) + 1
-#             # R[i_1, j_1, 1] = choices.argmin()
-#             R[i_1, j_1] = choices.min()
-#     # now we do the backward algorithm
-#     counts = {0: 0, 1: 0, 2: 0}  # indices correspond to choices
-#     i, j = R.shape
-#     i -= 1
-#     j -= 1
-#     while i > 0 or j > 0:
-#         i_1 = i - 1
-#         j_1 = j - 1
-#         if i > 0 and R[i_1, j] == R[i, j] - 1:
-#             counts[0] += 1  # deletion
-#             i -= 1
-#         elif j > 0 and R[i, j_1] == R[i, j] - 1:
-#             counts[1] += 1  # insertion
-#             j -= 1
-#         elif i > 0 and j > 0 and R[i_1, j_1] == R[i, j] - 1:
-#             counts[2] += 1
-#             i -= 1
-#             j -= 1
-#         elif i > 0 and j > 0 and R[i_1, j_1] == R[i, j]:
-#             i -= 1
-#             j -= 1
-#         # match_type = R[i, j, 1]
-#         # # extra check to see if its a match or a substitute
-#         # to_add = 0 if match_type == 2 and r[i-1] == h[i-1] else 1
-#         # counts[match_type] += to_add
-#         # i_dec = 0 if match_type == 1 else 1  # we don't decrement i if insert
-#         # j_dec = 0 if match_type == 0 else 1  # we don't decrement j if delete
-#         # i -= i_dec
-#         # j -= j_dec
-#
-#     # return, WER, followed by counts, mine are in reverse order
-#     return [R[-1, -1] / (len(R) - 2)] + [counts[i] for i in reversed(range(3))]
-
-
 def process_line(l):
+    """
+
+    :param l: a line as string
+    :return: string with all punctuation removed.
+    """
     return re.sub(r"[^a-z\[\] ]+", "", l.lower()).split()
 
 
 def make_print(speaker, t, i, out):
+    """ Make Levenshtein output easily printable.
+
+    :param speaker: current speaker
+    :param t: type
+    :param i: position
+    :param out: Levenshtein output
+    :return: args for .format
+    """
     beg = [speaker, t, i, out[0]]
     prefixes = ['S:{}', 'I:{}', 'D:{}']
     return beg + list([p.format(o) for p, o in zip(prefixes, out[1:])])
@@ -174,9 +133,6 @@ if __name__ == "__main__":
                     continue
                 r, g, k = opened_transcripts
                 r = process_line(r)
-                print(r)
-                print(process_line(g))
-                print(process_line(k))
                 goog = Levenshtein(r, process_line(g))
                 g_lev.append(goog)
                 goog = make_print(speaker, 'Google', i, goog)
@@ -184,11 +140,6 @@ if __name__ == "__main__":
                 k_lev.append(kald)
                 kald = make_print(speaker, 'Kaldi', i, kald)
                 s = " ".join(["{}" for _ in range(7)])
-                print(s.format(*goog))
-                print(s.format(*kald))
-                break
-            break
-        break
     g_lev = np.array(g_lev)
     k_lev = np.array(k_lev)
     out1 = f"Google has a mean of: {np.mean(g_lev[:, 0])} " \
