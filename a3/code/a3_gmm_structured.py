49c2c339f23ac4a868b04e17aa139f997d24cf41
Christopher Choquette Choo <choquette.christopher@gmail.com>
2020-04-10 12:02:42 -0400

    update

--- a3/code/a3_gmm_structured.py
+++ a3/code/a3_gmm_structured.py
@@ -148,7 +148,7 @@ def logLik(log_Bs, myTheta):
     alllog = log_Bs + np.log(myTheta.omega)
     logmax = alllog.max(axis=0, keepdims=True)
     # print(f"alllog: {alllog}, logmax: {logmax}")
-    return np.mean(logmax + np.log(sumexp(alllog, logmax)))
+    return (logmax + np.log(sumexp(alllog, logmax))).sum()
 
 
 def train(speaker, X, M=8, epsilon=0.0, maxIter=20):
